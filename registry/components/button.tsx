41833b35b9791202ae494bba9411512d47cdbb17
Juan Manuel Cuellar <juan.cuellar@becall.local>
2025-04-08 15:54:19 +0200

    Added support for extended motion props

--- registry/components/button.tsx
+++ registry/components/button.tsx
@@ -1,17 +1,31 @@
-import { motion } from 'motion/react';
+import { HTMLMotionProps, motion } from 'motion/react';
 import { cn } from '../../src/lib/utils/cn';
 
-export function Button() {
+type Props = {
+    color: 'primary' | 'brand' | 'danger';
+    variant: 'solid' | 'outline' | 'ghost';
+    onlyIcon: boolean;
+} & HTMLMotionProps<'button'>;
+
+export function Button({ color, variant, onlyIcon, className, children, ...props }: Props) {
+
     return (
         <motion.button
             className={cn(
-
+                'flex items-center justify-center rounded-xl h-9 px-5 font-medium cursor-pointer',
+                'bg-indigo-600 text-white',
+                className
             )}
-            whileHover={{ scale: 1.03 }}
-            whileTap={{ scale: 0.97 }}
-            onClick={() => alert('Button clicked!')}
+            {...props}
+            whileHover={{
+                y: -3,
+            }}
+            whileTap={{
+                y: 3,
+            }}
+            animate={{ y: 0 }}
         >
-            Click me
+            {children}
         </motion.button>
     );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Button } from '../registry/components/button';
 export default function App() {
   return (
     <main className='p-10'>
-      <Button />
+      <Button>
+        <span>Click me</span>
+      </Button>
     </main>
   )
 }
