58d7e7cd42a2ad42750a265a4f4f77765a5c1ce4
Juan Manuel Cuellar <juan.cuellar@becall.local>
2025-04-09 15:28:08 +0200

    Added button component

--- registry/components/button.tsx
+++ registry/components/button.tsx
@@ -2,28 +2,35 @@ import { HTMLMotionProps, motion } from 'motion/react';
 import { cn } from '../../src/lib/utils/cn';
 
 type Props = {
-    color: 'primary' | 'brand' | 'danger';
-    variant: 'solid' | 'outline' | 'ghost';
-    onlyIcon: boolean;
+    color?: 'primary' | 'brand' | 'danger';
+    variant?: 'solid' | 'outline' | 'ghost';
+    onlyIcon?: boolean;
 } & HTMLMotionProps<'button'>;
 
-export function Button({ color, variant, onlyIcon, className, children, ...props }: Props) {
-
+export function Button({ color = 'brand', variant = 'solid', onlyIcon, className, children, ...props }: Props) {
     return (
         <motion.button
             className={cn(
-                'flex items-center justify-center rounded-xl h-9 px-5 font-medium cursor-pointer',
-                'bg-indigo-600 text-white',
+                'flex items-center justify-center',
+                'h-9 px-5',
+                'rounded-xl cursor-pointer',
+                'font-medium text-sm',
+                color === 'brand' && (
+                    variant === 'outline' ? 'bg-white outline outline-indigo-600 hover:outline-indigo-700 text-indigo-600 hover:text-indigo-700' :
+                        variant === 'ghost' ? 'bg-white text-indigo-600 hover:text-indigo-700' :
+                            variant === 'solid' && 'bg-indigo-600 hover:bg-indigo-700 text-white'),
+                color === 'danger' && (
+                    variant === 'outline' ? 'bg-white outline outline-rose-600 hover:outline-rose-700 text-rose-600 hover:text-rose-700' :
+                        variant === 'ghost' ? 'bg-white text-rose-600 hover:text-rose-700' :
+                            variant === 'solid' && 'bg-rose-600 hover:bg-rose-700 text-white'),
+                color === 'primary' && (
+                    variant === 'outline' ? 'bg-white outline outline-zinc-900 hover:outline-zinc-800 text-zinc-900 hover:text-zinc-800' :
+                        variant === 'ghost' ? 'bg-white text-zinc-900 hover:text-zinc-800' :
+                            variant === 'solid' && 'bg-zinc-900 hover:bg-zinc-800 text-white'),
+                onlyIcon && 'px-0 w-9',
                 className
             )}
             {...props}
-            whileHover={{
-                y: -3,
-            }}
-            whileTap={{
-                y: 3,
-            }}
-            animate={{ y: 0 }}
         >
             {children}
         </motion.button>
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import { Button } from '../registry/components/button';
 
 export default function App() {
+
+  const colors = ['primary', 'brand', 'danger'] as const;
+
   return (
-    <main className='p-10'>
-      <Button>
-        <span>Click me</span>
-      </Button>
+    <main className='p-10 flex gap-10'>
+      {colors.map((color) => (
+        <section key={color} className='flex flex-col items-center gap-5 max-w-50 w-full'>
+          <Button color={color} variant='solid'>
+            <span>Click me</span>
+          </Button>
+          <Button color={color} variant='outline'>
+            <span>Click me</span>
+          </Button>
+          <Button color={color} variant='ghost'>
+            <span>Click me</span>
+          </Button>
+          <Button color={color} variant='solid' onlyIcon>
+            <span>A</span>
+          </Button>
+          <Button color={color} variant='outline' onlyIcon>
+            <span>A</span>
+          </Button>
+          <Button color={color} variant='ghost' onlyIcon>
+            <span>A</span>
+          </Button>
+        </section>
+      ))}
     </main>
   )
 }
